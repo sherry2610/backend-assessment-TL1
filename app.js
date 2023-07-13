8a7a4eeba7b9f1d750afe07b9cd441061743bde1
sherry2610 <shaharyarkhalid2610@gmail.com>
2023-07-14 03:32:54 +0500

    fix: add start and dev scripts

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ connectDB();
 
 const app = express();
 
-// view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +19,6 @@ app.use("/", usersRouter);
 app.use("/admin", questionsRouter);
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
